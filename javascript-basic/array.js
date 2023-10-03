7d30e664a82206e9a2bf412fa1d4686008f9d5de
hairunisa0929 <hairunisa.learning@gmail.com>
2023-10-03 11:41:28 +0700

    update javascript basic

--- javascript-basic/array.js
+++ javascript-basic/array.js
@@ -3,10 +3,10 @@ let food = [];
 food = ["pizza", "burger", "fries"];
 
 let animals = ["cat", "dog"];
-console.log(typeof animals);
+// console.log(typeof animals);
 
 let arr1 = ["kelinci", 4, false, [1, 2, 3]];
-console.log(arr1[3][2]);
+// console.log(arr1[3][2]);
 
 // let bulan1 = "januari";
 // let bulan2 = "februari";
@@ -14,19 +14,21 @@ console.log(arr1[3][2]);
 let months = ["januari", "februari", "maret", "april"];
 
 for (let i = 0; i < months.length; i++) {
-    console.log(months[i]);
+//   console.log(months[i]);
 }
 
 for (let month of months) {
-    console.log(month);
+//   console.log(month);
 }
 
 // splice
-const veggies = ["Spinach", "Carrot", "Potato", "Cabbage"];
-fruits.splice(0,2);
-console.log(veggies)
+const veggies = ["Spinach", "Carrot", "Potato", "Cabbage", "Broccoli"];
+// menghapus 2 elemen di index 0
+veggies.splice(0, 2);
+// console.log("veggies after splice", veggies);
 
 // slice
 const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-const citrus = fruits.slice(1, 4);
+// Mengambil elemen index 1 sampai index 2
+const citrus = fruits.slice(1, 3);
 console.log(citrus);
